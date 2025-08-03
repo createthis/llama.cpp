fb1d48d8d0cd52778e52ddd92be56331edf663d7
Sam <sammcj@users.noreply.github.com>
2025-08-03 17:57:05 +1000

    Apply suggestion from @CISC
    Co-authored-by: Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>

--- src/llama-arch.cpp
+++ src/llama-arch.cpp
@@ -1404,7 +1404,6 @@ static const std::map<llm_arch, std::map<llm_tensor, const char *>> LLM_TENSOR_N
             { LLM_TENSOR_ATTN_OUT,           "blk.%d.attn_output" },
             { LLM_TENSOR_ATTN_Q_NORM,        "blk.%d.attn_q_norm" },
             { LLM_TENSOR_ATTN_K_NORM,        "blk.%d.attn_k_norm" },
-            { LLM_TENSOR_FFN_NORM,           "blk.%d.ffn_norm" },
             { LLM_TENSOR_FFN_GATE,           "blk.%d.ffn_gate" },        // dense layers
             { LLM_TENSOR_FFN_DOWN,           "blk.%d.ffn_down" },        // dense layers
             { LLM_TENSOR_FFN_UP,             "blk.%d.ffn_up" },          // dense layers
