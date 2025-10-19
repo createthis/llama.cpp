347eb48ddf18c3e1574cefdd6dcc169ad90dfcb4
Jesse CreateThis <jesse@createthis.com>
2025-10-18 22:49:16 -0400

    Attempt to fix "not enough space in the context's memory pool" error.

--- src/llama-context.cpp
+++ src/llama-context.cpp
@@ -1362,7 +1362,11 @@ void llama_context::output_reorder() {
 //
 
 uint32_t llama_context::graph_max_nodes() const {
-    return std::max<uint32_t>(1024u, 8u*model.n_tensors());
+    uint32_t base = std::max<uint32_t>(1024u, 8u*model.n_tensors());
+    if (model.arch == LLM_ARCH_DEEPSEEK3_2) {
+        base = std::max<uint32_t>(base, 16u*model.n_tensors());
+    }
+    return base;
 }
 
 llm_graph_result * llama_context::get_gf_res_reserve() const {
